f692b3a5f036898923adbc479d63651557863c72
ti360 <ti.rvalley@gmail.com>
2022-11-10 10:55:17 +0900

    First Release

--- README.md
+++ README.md
@@ -9,38 +9,46 @@ The idea for the project came entirely from the following video:
 ## Description
 It has already been said 200 times that Saitek Trim is an Axis and therefore does not sync with the state in simulator.
 
-Autopilot, digital trim, or any other state change results in a sudden change in trim.
+Any autopilot, digital trim, or other background change of state will cause an abrupt attitude change on the next SaitekTrim input.
 
-Peter introduces the idea of hi-jacking the output of the SaitekTrim into a microcontroller and using V-USB as an input for USB HID.
+As a countermeasure, the output of SaitekTrim is hijacked and input to the microcontroller, and V-USB is used as the input for USB HID.
 
 He programmed it using an ATtiny85, but we can use Digispark and DigiJoystick libraries.
 
 ## Getting Started
 
 ### Prerequisite
+
 **Wiring to hacked Saitek Trim and Digispark is required.**
 
 See the video [Hacking the Saitek Trim Wheel (Twin Otter Cockpit Build - Part 11)](https://youtu.be/SSh6FKWpMg0)
 
-We need just two wires from the SaitekTrim's encoder pins to the Digispark pins.
+
+We need just two wires from the SaitekTrim's encoder pins to the Digispark input pins.
+
+One for PIN0 or PIN1 (depending on your device) and the other for PIN2.
 
 ### Installing
+
 Load this sketch to your Arduino IDE and transfer to your Digispark device.
 
 
 ## Help
 
 * You can change PIN assign for your own Digispark models.
+
 http://digistump.com/wiki/digispark/tutorials/modelbi2c
 
+Mine has a built-in LED on pin 1, but if yours is pin 0, please swap the PIN_A and PIN_LED assignments (and also the wiring).
+
 ```
-const unsigned char PIN_A = 0;
-const unsigned char PIN_B = 2;
+const uint8_t PIN_LED = 1;
+const uint8_t PIN_A = 0;   
 ```
 
-* And button release interval
+* You can change button release interval
 ```
-const unsigned char BUTTON_WAIT = 5;  // in milliseconds.
+const uint8_t BUTTON_WAIT = 17;  // in milliseconds. 60fps=16.66ms
 ```
 
 
--- trimkey.ino
+++ trimkey.ino
@@ -1,21 +1,22 @@
-#include <DigiJoystick.h>
+#include "DigiJoystick.h"
 
-const unsigned char PIN_LED = 1;
-const unsigned char PIN_A = 0;
-const unsigned char PIN_B = 2;
-const unsigned char BUTTON_WAIT = 5;  // in milliseconds.
 
-static unsigned char prev_signal = 0;
-static unsigned char signal = 0;
-static unsigned char button_counter = 0;
+const uint8_t PIN_LED = 1;
+const uint8_t PIN_A = 0;
+const uint8_t PIN_B = 2;
+const uint8_t BUTTON_WAIT = 17;  // in milliseconds. 60fps=16.66ms
+
+static uint8_t change_signal = 0;
+static uint8_t prev_signal = 0;
+static uint8_t button_counter = 0;
+static bool direction = true;
+
 
 void setup() {
   pinMode(PIN_A, INPUT);
   pinMode(PIN_B, INPUT);
   pinMode(PIN_LED, OUTPUT);
   // register setting.
-  PCMSK |= ((1 << PIN_A) | (1 << PIN_B));  // Enable encoder pins interrupt sources
-  GIMSK |= (1 << PCIE);                    // Enable pin change interupts
   TIMSK |= (1 << TOIE0);                   // Enable timer interrupts
   TCCR0B |= ((1 << CS00) | (1 << CS01));   // 16.5Mhz/64 prescaling = 257.8Khz = 0.0039ms
   sei();                                   // enable global interrupt.
@@ -23,66 +24,80 @@ void setup() {
   // set initial value
   TCNT0 = 0x00;                                        // Timer0: overflow(interrupt) every 0.039ms*256 = about 1ms.
   prev_signal = PINB & ((1 << PIN_A) | (1 << PIN_B));  // PIN inputs.
-  attachInterrupt(PCINT0, intHandler, CHANGE);
+
 }
 
 
-void loop() {
-  // do nothing.
-}
-
-void push_button(char button) {
+void push_button(bool direction) {
   if (button_counter == 0) {
-    DigiJoystick.setButtons(button, (char)0x00);
+    if (direction){
+      DigiJoystick.setButtons((char)0x01, (char)0x00);
+    } else {
+      DigiJoystick.setButtons((char)0x02, (char)0x00);
+    }
     button_counter = BUTTON_WAIT;
   }
 }
 
-
-void intHandler() {
-  // PinChangeINTerrupt
-  /* input signal pattern is:
+void loop() {
+  // Unfortunately, Digispark only has one external interrupt and we can't use interrupts on both encoder pins.
+  // Also, V-USB devours all pin change interrupts, so we can't use PCINT0_vect too.
+  // So we use the classic loop system.
+  /* input signal pattern
           positive ->
+       <- negative
   PIN_A: 001100110011
   PIN_B: 011001100110
-         <- negative
 
   so matrix is:
    now      prev
    A+B    pos | neg
-   0+1 <- 0+0 | 1+1
-   1+1 <- 0+1 | 1+0
    0+0 <- 1+0 | 0+1 
+   0+1 <- 0+0 | 1+1
    1+0 <- 1+1 | 0+0
+   1+1 <- 0+1 | 1+0
+
   */
 
-  signal = PINB & ((1 << PIN_A) | (1 << PIN_B));
-  switch (signal & (1 << PIN_A)) {  //PIN_A
-    case 0:
-      if (signal & (1 << PIN_B)) {  //PIN_B
-        push_button((char)0x02);
-      } else {
-        push_button((char)0x01);
-      }
-      break;
-    case 1:
-      if (signal & (1 << PIN_B)) {  //PIN_B
-        push_button((char)0x01);
-      } else {
-        push_button((char)0x02);
-      }
-      break;
+  uint8_t signal = PINB & ((1 << PIN_A) | (1 << PIN_B));
+  change_signal = signal ^ prev_signal;
+  if ( change_signal == 0 ){ // same input.
+    return;
   }
+
+  if ( change_signal & ( 1<< PIN_A) ) { // change A
+    if ( 
+        ( ((prev_signal & (1 << PIN_B)) >> PIN_B ) & 0x01 ) // prev B bit
+        ^  ( ((signal & (1 << PIN_A)) >> PIN_A ) & 0x01 )   // now A bit
+       ) { // PIN_A flag != prev PIN_B flag = negative
+      direction = false;
+    } else {
+      direction = true;
+    }
+  } else { //change B
+    if (
+        ( ((prev_signal & (1 << PIN_A)) >> PIN_A ) & 0x01 ) // prev A bit
+        ^  ( ((signal & (1 << PIN_B)) >> PIN_B ) & 0x01 )   // now B bit
+       ) { // PIN_B flag != prev PIN_A flag = positive
+      direction = true;
+    } else {
+      direction = false;
+    }
+  }
+  prev_signal = signal;
+  push_button(direction);
 }
+
 
 ISR(TIMER0_OVF_vect) {
   // Called every 1ms. (16.5Mhz/64 prescaling = 257.8Khz = 0.0039ms. 0.0039ms * 256 = 1ms)
   if (button_counter) {
     button_counter--;
-    digitalWrite(PIN_LED, HIGH); // for debug.
+    digitalWrite(PIN_LED, HIGH); // for debug
   } else {
-    DigiJoystick.setButtons((char)0x00, (char)0x00);  // button release.
+    DigiJoystick.setButtons((char)0x00, (char)0x00);  // release button 
     digitalWrite(PIN_LED, LOW);
   }
   DigiJoystick.update();
+
 }
